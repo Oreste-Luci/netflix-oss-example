bf3622e31618c0492c5ca70c49a2f3e244dc2545
Oreste Luci <oreste@luci.cl>
2015-05-24 21:36:14 +0200

    Updating Configuration

--- config-service/src/main/java/service/config/boot/ConfigService.java
+++ config-service/src/main/java/service/config/boot/ConfigService.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * @author Oreste Luci
  */
-@Configuration
 @EnableAutoConfiguration
 @EnableDiscoveryClient
 @EnableConfigServer

41712654057ed354a2fbe6d3a42e48ad576e2283
Oreste Luci <oluci@oneboxtm.com>
2015-05-28 15:33:48 +0200

    Updating config.

--- service_a/src/main/java/service/a/boot/ApplicationA.java
+++ service_a/src/main/java/service/a/boot/ApplicationA.java
@@ -1,6 +1,7 @@
 package service.a.boot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -10,11 +11,12 @@ import org.springframework.context.annotation.ComponentScan;
 /**
  * @author Oreste Luci
  */
+@EnableAutoConfiguration
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableCircuitBreaker
 @EnableFeignClients(value = "service.a.*")
-@ComponentScan(value = "service.a.*")
+@ComponentScan("service.a.*")
 public class ApplicationA {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationA.class, args);

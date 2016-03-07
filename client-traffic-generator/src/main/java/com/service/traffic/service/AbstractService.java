d60c0a8dc6911e1c19b4b6bf9a7bdeddf9bee665
Oreste Luci <oreste.luci@gmail.com>
2016-03-07 17:36:14 +0100

    Remove traffic generator

--- client-traffic-generator/src/main/java/com/service/traffic/service/AbstractService.java
+++
@@ -1,9 +0,0 @@
-package com.service.traffic.service;
-
-/**
- * Created by mnavarro on 25.06.15.
- */
-public abstract class AbstractService {
-
-    public static final String CLIENT_SERVICE = "serviceA";
-}
